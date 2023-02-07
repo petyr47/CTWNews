b6768d6f2c9acbfe149c68416500e8ed6d43e5ed
Aneke Peter Mark <anekepeter47@gmail.com>
2023-02-07 17:23:33 +0000

    add parcelize

--- app/src/main/java/com/aneke/peter/ctwnews/network/model/Source.kt
+++ app/src/main/java/com/aneke/peter/ctwnews/network/model/Source.kt
@@ -1,6 +1,10 @@
 package com.aneke.peter.ctwnews.network.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Source(
     val id: String = "",
     val name: String = ""
-)
+) : Parcelable
